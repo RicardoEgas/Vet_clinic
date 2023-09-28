beadffc08bfcc92745779b8ea7c8cefb3f980748
RicardoEgas <rj_egas@hotmail.com>
2023-09-28 02:19:27 +0200

    update table and queries

--- queries.sql
+++ queries.sql
@@ -15,3 +15,59 @@ SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3 ;
+
+BEGIN;
+
+UPDATE animals SET species = 'unspecified' ;
+
+SELECT name, species from animals;
+
+ROLLBACK;
+
+SELECT name, species from animals;
+
+BEGIN;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+COMMIT;
+
+SELECT name , species from animals;
+
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT delete_all_born_after_Jan1st22;
+
+UPDATE animals SET weight_kg = weight_kg * -1 ;
+
+ROLLBACK TO delete_all_born_after_Jan1st22;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0 ;
+
+COMMIT;
+
+SELECT name , weight_kg from animals;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) as total_escape FROM animals GROUP BY neutered ORDER BY total_escape DESC LIMIT 1;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
