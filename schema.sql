9d50e7f5cb35545f1d178edccd6f2489901ed43c
RicardoEgas <rj_egas@hotmail.com>
2023-09-29 01:53:35 +0200

    Add multiple tables and queries

--- schema.sql
+++ schema.sql
@@ -11,3 +11,21 @@ CREATE TABLE animals (
 
 ALTER TABLE animals 
 ADD COLUMN species VARCHAR
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INTEGER;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
